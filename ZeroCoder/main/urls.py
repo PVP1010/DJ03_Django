a85c9c81b3b8b10ceea218159bc0dd7a8f5a7da3
PVP1010 <Valera10102791@mail.ru>
2025-02-01 14:03:17 +0700

    add

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+# присваеваем имена адресам страниц
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new)
+    path('', views.index, name='home'), # добавляем имя страницы home
+    path('new/', views.new, name='page2'), # добавляем имя страницы page2
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html')
+    data={'caption': 'Cosmos!'}
+    return render(request, 'main/index.html', data)
 
 def new(request):
     return render(request, 'main/new.html')
