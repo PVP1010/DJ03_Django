01de81803d5bdad9152e4dc9714d766e3113d3bd
PVP1010 <Valera10102791@mail.ru>
2025-02-04 01:02:26 +0700

    add

--- ZeroCoder/news/urls.py
+++ ZeroCoder/news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # присваеваем имена адресам страниц
 urlpatterns = [
-    path('', views.home, name='news_home'), # добавляем имя страницы
-    path('create_news/', views.create_news, name='add_news'),
+    path('', views.home, name='news_home'),                        # добавляем имя страницы
+    path('create_news/', views.create_news, name='add_news'),      # добавляем имя страницы add_news
 ]
+
