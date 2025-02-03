d499e05312550ea13cd15df8a43cee0eabee2192
PVP1010 <Valera10102791@mail.ru>
2025-02-04 00:09:17 +0700

    add

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render                            # импортируем функцию render
+from django.shortcuts import render, redirect                            # импортируем функцию render и функцию redirect
 from .models import News_post                                  # импортируем модель News_post
 from .forms import News_postForm                                # импортируем форму
 # Create your views here.
@@ -8,5 +8,13 @@ def home(request):
     return render(request, 'news/news.html', {'news': news})  # передаем их в виде словаря news.
 
 def create_news(request):
+    error = ''
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            form.save()                                      # сохраняем новость
+            return redirect('news_home')                     # перенаправляем на главную страницу
+        else:
+            error = "Данные были заполнены некорректно"
     form = News_postForm()
-    return render(request, 'news/add_new_post.html', {'form': form})
+    return render(request, 'news/add_new_post.html', {'form': form, 'error': error})
